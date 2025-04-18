84f7595f46c5f67ae9fae7a1d510335e1ca64b85
khanh <vukhanh.003@gmail.com>
2025-04-18 21:21:57 +0700

    fix

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use('/api/categories', require('./routes/categoryRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/cart', require('./routes/cartRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
+app.use('/api/notifications', require('./routes/notificationRoutes'));
 
 // Error handling middleware
 app.use((err, req, res, next) => {
